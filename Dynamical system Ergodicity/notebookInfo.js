db10dc6269e578aa380cab77a3ee6d949862bbad
JingMatrix <jingmatrix@gmail.com>
2021-02-11 00:06:07 +0100

    add backup

--- Dynamical system Ergodicity/notebookInfo.js
+++ Dynamical system Ergodicity/notebookInfo.js
@@ -1,3 +1,3 @@
 var staticUrl = 'http://new.notesplusapp.com/static';
 var notebook = {'name' : 'Dynamical system Ergodicity',
-                'pageCount' : 54 };
+                'pageCount' : 69 };
